d2c0de9e5351d55247d03ba1716239b8a2114c76
MinKyu Tae <alsrb8442@naver.com>
2022-12-14 02:25:38 +0900

    practice ts-redux-counter

--- ts-tutorial/src/App.tsx
+++ ts-tutorial/src/App.tsx
@@ -1,16 +1,9 @@
 import React from "react";
 import "./App.css";
-import TodoForm from "./components/TodoForm";
-import TodoList from "./components/TodoList";
-import { TodosContextProvider } from "./contexts/TodosContext";
+import Counter from "./components/Counter";
 
 const App = () => {
-  return (
-    <TodosContextProvider>
-      <TodoForm />
-      <TodoList />
-    </TodosContextProvider>
-  );
+  return <Counter />;
 };
 
 export default App;
