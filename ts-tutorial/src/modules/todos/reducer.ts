ada9fa84cefb20ac35daf8bada7054ff01149b0b
MinKyu Tae <alsrb8442@naver.com>
2022-12-20 23:47:01 +0900

    practice ts-redux-refactoring

--- ts-tutorial/src/modules/todos/reducer.ts
+++ ts-tutorial/src/modules/todos/reducer.ts
@@ -1,24 +1,6 @@
-import { createAction, ActionType, createReducer } from "typesafe-actions";
-
-// 액션 type
-const ADD_TODO = "todos/ADD_TODO";
-const TOGGLE_TODO = "todos/TOGGLE_TODO";
-const REMOVE_TODO = "todos/REMOVE_TODO";
-
-// 액션 생성 함수
-export const addTodo = createAction(ADD_TODO)<string>();
-export const toggleTodo = createAction(TOGGLE_TODO)<number>();
-export const removeTodo = createAction(REMOVE_TODO)<number>();
-
-const actions = { addTodo, toggleTodo, removeTodo };
-type TodosAction = ActionType<typeof actions>;
-
-export type Todo = {
-  id: number;
-  text: string;
-  done: boolean;
-};
-type TodosState = Todo[];
+import { createReducer } from "typesafe-actions";
+import { ADD_TODO, REMOVE_TODO, TOGGLE_TODO } from "./actions";
+import { TodosAction, TodosState } from "./types";
 
 const initialState: TodosState = [
   { id: 1, text: "타입스크립트 배우기", done: true },
